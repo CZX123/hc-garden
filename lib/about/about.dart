8ee8dc700f9173aa78136de42cc90c1ac1662056
asdiml <tanteongseng2@gmail.com>
2019-11-01 10:36:24 +0800

    Minor changes to the History and About pages

--- lib/about/about.dart
+++ lib/about/about.dart
@@ -1,5 +1,28 @@
 import '../library.dart';
 
+String ackParas = "This project would certainly not have been possible without the generous assistance of many of our Hwa Chong teachers, alumni and students. \n \n"
+      "We would like to express our heartfelt appreciation and sincere gratitude to our Principal, Mr. Pang Choon How, and Deputy Principals for their support, in particular: Deputy Principal/Special Projects, Mr Tan Pheng Tiong, for sharing with us more about Hwa Chong's rich history and landscaping, as well as the historical photographs of the school; "
+      "Dean of Research Studies, Dr Chia Hui Ping, for sharing her expertise and knowledge in botany with us; Dr Lim Jit Ning, for his endless encouragement; Dr Adeline Chia, for her guidance and mentorship throughout this entire journey; Dr Sandra Tan, for her invaluable recommendations and assistance in the publicity of the project, as well as vetting of write-ups; "
+      "Mr Tang Koon Loon and Mr Lau Soo Yen, for sharing their passion for birds and vivid photographs of fauna in the school with us; Mr Mark Tan, for his innovative suggestions, comprehensive proofreading of the write-ups and training us as trail guides; together with Ms Tan Wei Qian, for her constructive suggestions concerning the write-ups. \n \n"
+      "We would also like to extend our gratitude to school alumni who have contributed to our school landscape. In particular to Mr. Mak Chin On, for the contributions of fauna that he has made over the past few decades, and for sharing his wealth of knowledge on the great variety of trees in the school with us. \n \n"
+      "The estate staff and school gardeners are our unsung heroes - working tirelessly over many years to shape the landscape in school, and making it the thriving Garden Campus it is today. \n \n"
+      "Last but not least, we would like to thank the school for providing us with the opportunity to work on this very meaningful project. This journey has taught us to appreciate Hwa Chong from a different perspective, and proves that this hundred year-old campus still has many surprises waiting to be uncovered. \n\n"
+      "This work is dedicated to all the giants in our lives.";
+String refsPara = "We would also like to acknowledge the following key sources of information for our writeups. We declare that no photographs from these sources have been used by us. \n \n"
+      "Chin, Jacquelin, and John Chin. John&Jacq\'s Garden. 2011. Accessed 2019. https://jaycjayc.com/ \n \n"
+      "Fern, Ken et al. \"Useful Tropical Plants.\" Useful Tropical Plants. Accessed 2019. http://tropical.theferns.info/ \n \n"
+      "Flowers of India. Accessed 2019. http://flowersofindia.net/ \n \n"
+      "Master Gardener Program. 2019. Accessed 2019. https://wimastergardener.org/ \n \n"
+      "Missouri Botanical Garden. Accessed 2019. http://www.missouribotanicalgarden.org/ \n \n"
+      "Mound, Laurence A, Dom W Collins and Anne Hastings. Thysanoptera Britannica et Hibernica - Thrips of The British Isles. Lucidcentral.org, Identic Pty Ltd, Queensland, Australia. 2018. Accessed 2019. https://keys.lucidcentral.org \n \n"
+      "National Center for Biotechnology Information (NCBI). Accessed 2019. https://www.ncbi.nlm.nih.gov/ \n \n"
+      "National Library Board Singapore eResources. 2019. Accessed 2019. http://eresources.nlb.gov.sg/ \n \n"
+      "National Parks Board (NParks). Accessed 2019. https://www.nparks.gov.sg/ \n \n"
+      "Plants For A Future. 2012. Accessed 2019. https://pfaf.org/user/Default.aspx \n \n"
+      "Silk, Ferry et al. Plants of Southeast Asia. 2009. Accessed 2019. http://www.asianplant.net/ \n \n"
+      "Stuart, Godofredo U. StuartXchange. 2018. Accessed 2019. http://www.stuartexchange.org/ \n \n"
+      "Wikipedia. Accessed 2019. https://en.wikipedia.org/";
+
 List<String> aboutPageTitles = [
   'Message from Committee',
   'Acknowledgements',
@@ -7,15 +30,15 @@ List<String> aboutPageTitles = [
 ];
 
 List<String> aboutPageQuotes = [
-  '\'If you truly love Nature, you will find beauty everywhere.\' \n- Vincent van Gogh',
-  '\'If I have seen further, it is by standing on the shoulders of giants.\' \n- Sir Isaac Newton',
-  '\'The larger the island of knowledge, the longer the shoreline of wonder.\' \n- Ralph W. Sockman',
+  '\'If you truly love Nature, you will find beauty everywhere.\' ~Vincent van Gogh',
+  '\'If I have seen further, it is by standing on the shoulders of giants.\' ~Sir Isaac Newton',
+  '\'The larger the island of knowledge, the longer the shoreline of wonder.\' \n~Ralph W. Sockman',
 ];
 
 List<String> aboutPageDes = [
   'As a group of nature lovers in Hwa Chong, we set out to discover the legacy of beauty in the greenery of our garden campus. Identifying the plethora of flora and fauna seemed daunting at first, but we ended up deeply enthralled by the boundless biodiversity in our school and the rich history behind it. \n \nWe were also amazed by the amount of devotion behind the landscape of Hwa Chong - some plants are donations from alumni, while others are selections by teachers for biology classes. The healthy ecosystem created by the artfully designed green spaces allowed a great variety of birds and insects to thrive on campus, bringing us closer to nature. \n \nIndeed, the excitement of identifying species and learning about the intriguing characteristics of each of them drove us to become more curious and observant each time we wander around in school. Every time we pause and marvel at the plants around us, we become more determined to inspire our friends to appreciate the beauty around us. With our website and mobile app that present selected flora and fauna on campus, we hope that you would also take the time to wander, and pay attention to the hidden gems on our garden campus. ',
-  'Praesent ac dolor vestibulum, egestas erat nec, vestibulum velit. Mauris ut mauris at orci gravida dapibus. Morbi posuere nulla est, ac interdum arcu semper sit amet. Cras porttitor gravida mi, non condimentum turpis accumsan eu. Ut laoreet nisi at est convallis sodales. Donec lacinia, dolor rhoncus molestie consectetur, felis ex imperdiet nulla, nec rhoncus dui massa sed lacus. Nunc a interdum sapien. Morbi imperdiet tellus ut nulla placerat maximus. Maecenas facilisis urna ut consectetur fermentum. Etiam elementum tristique quam, non fermentum quam euismod eu. Quisque cursus faucibus metus at egestas.',
-  'Cras sem ipsum, efficitur vitae cursus sed, molestie ac sem. Donec non vulputate arcu. Duis efficitur gravida ullamcorper. Nunc orci ligula, efficitur eu maximus ac, molestie ut elit. Nullam eu nisl nec lacus ultricies fringilla a sed justo. In enim purus, pretium nec tempus id, elementum nec elit. Aliquam hendrerit mi quis est tincidunt fringilla. In condimentum lorem gravida ex luctus, non interdum magna tempus. Vestibulum ut ante suscipit, imperdiet enim ac, vestibulum sem. Sed sit amet tempus eros. Proin eget sodales velit, et rutrum nisi.',
+  ackParas,
+  refsPara,
 ];
 
 class AboutPageItem {
@@ -63,7 +86,7 @@ class _AboutPageState extends State<AboutPage> {
             title: Text(
               'About',
               style: Theme.of(context).textTheme.display1.copyWith(
-                    //color: Colors.brown,
+                    color: Colors.brown,
                     fontSize: 22,
                     fontWeight: FontWeight.bold,
                   ),
@@ -88,14 +111,14 @@ class _AboutPageState extends State<AboutPage> {
                       child: Text(
                         item.title,
                         style: const TextStyle(
-                          fontSize: 16,
+                          fontSize: 17,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                     );
                   },
                   body: Padding(
-                      padding: const EdgeInsets.only(bottom: 24),
+                      padding: const EdgeInsets.only(bottom: 28),
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         crossAxisAlignment: CrossAxisAlignment.center,
@@ -129,75 +152,6 @@ class _AboutPageState extends State<AboutPage> {
               }).toList(),
             ),
           ),
-        ),
-      ],
-    );
-    return ListView(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Text(
-            'About',
-            style: Theme.of(context).textTheme.display1.copyWith(
-                  color: Colors.brown,
-                  fontSize: 32,
-                  fontWeight: FontWeight.w700,
-                ),
-            textAlign: TextAlign.center,
-          ),
-        ),
-        ExpansionPanelList(
-          expansionCallback: (int index, bool isExpanded) {
-            setState(() {
-              _aboutPageData[index].isExpanded = !isExpanded;
-            });
-          },
-          children: _aboutPageData.map((item) {
-            return ExpansionPanel(
-              canTapOnHeader: true,
-              headerBuilder: (BuildContext context, bool isExpanded) {
-                return Padding(
-                  padding: const EdgeInsets.only(top: 8, left: 16),
-                  child: Text(
-                    item.title,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                );
-              },
-              body: Padding(
-                  padding: const EdgeInsets.only(bottom: 26.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(26.0, 0, 26.0, 8.0),
-                        child: Text(
-                          item.quote,
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            color: Colors.grey,
-                            fontStyle: FontStyle.italic,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 0),
-                        child: Text(
-                          item.body,
-                          style: TextStyle(
-                            fontSize: 15,
-                          ),
-                        ),
-                      ),
-                    ],
-                  )),
-              isExpanded: item.isExpanded,
-            );
-          }).toList(),
         ),
       ],
     );
