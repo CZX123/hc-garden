905863f0f797423cd80e00b0ebb8c4837e742fe6
asdiml <tanteongseng2@gmail.com>
2019-11-11 16:50:03 +0800

    Location Overview Circles

--- lib/explore/trail/location_overview_page.dart
+++ lib/explore/trail/location_overview_page.dart
@@ -1,6 +1,6 @@
 import '../../library.dart';
 
-class TrailLocationOverviewPage extends StatelessWidget {
+class TrailLocationOverviewPage extends StatefulWidget {
   final Trail trail;
   final TrailLocation trailLocation;
   const TrailLocationOverviewPage({
@@ -10,14 +10,23 @@ class TrailLocationOverviewPage extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _TrailLocationOverviewPageState createState() => _TrailLocationOverviewPageState();
+}
+
+class _TrailLocationOverviewPageState extends State<TrailLocationOverviewPage> {
+  
+  double aspectRatio;
+  
+  @override
   Widget build(BuildContext context) {
     final topPadding = MediaQuery.of(context).padding.top;
     final height = MediaQuery.of(context).size.height;
-    final orientation = MediaQuery.of(context).orientation;
+    final width = MediaQuery.of(context).size.width;
+    //final orientation = MediaQuery.of(context).orientation;
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.landscapeLeft,
-      DeviceOrientation.landscapeRight
+      DeviceOrientation.landscapeRight,
     ]);
     return Scaffold(
       bottomNavigationBar: BottomAppBar(
@@ -34,7 +43,12 @@ class TrailLocationOverviewPage extends StatelessWidget {
               tooltip: 'Back',
             ),
             Expanded(
-              child: Text(trailLocation.name),
+              child: Text(
+                widget.trailLocation.name,
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
             ),
             SizedBox(width: 48),
           ],
@@ -46,12 +60,53 @@ class TrailLocationOverviewPage extends StatelessWidget {
           constraints: BoxConstraints(
             minHeight: height - 48,
           ),
-          child: CustomImage(
-            trailLocation.image,
-            fit: BoxFit.contain,
+          alignment: Alignment.center,
+          child: Stack(
+            children: <Widget>[
+              CustomImage(
+                widget.trailLocation.image,
+                fit: BoxFit.contain,
+                onLoad: (double aspectRatio) {
+                  setState(() {
+                    this.aspectRatio = aspectRatio;
+                  });
+                },
+              ),
+              for (var entityPosition in widget.trailLocation.entityPositions) 
+                new Positioned(
+                  left: entityPosition.left*width,
+                  top: imageHeight==null ? height : entityPosition.top*imageHeight,
+                  child: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.yellow
+                      ),
+                    ),
+                  ),
+                ),
+            ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class AnimatedPulseCircle extends StatefulWidget {
+  AnimatedPulseCircle({Key key}) : super(key: key);
+
+  @override
+  _AnimatedPulseCircleState createState() => _AnimatedPulseCircleState();
+}
+
+class _AnimatedPulseCircleState extends State<AnimatedPulseCircle> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Text("idk"),
     );
   }
 }
