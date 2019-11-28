7658c7307cad09fad5d512ddaba739d587973eeb
weiqilu <luweiqi@hotmail.sg>
2019-11-28 20:13:19 +0800

    iOS configurations

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,10 +1,14 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
+@import Firebase;
+@import GoogleMaps;
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [FIRApp configure];
+  [GMSServices provideAPIKey:@"AIzaSyD-mmzS2c9AcbKfT5Y4UhHypE5GiG7Bgqw"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
